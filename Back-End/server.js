35eef3958249e45c203422d4da1b048e984597cf
unknown <eyaabaab11@gmail.com>
2025-02-19 12:09:47 +0100

    Project running

---
+++ Back-End/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+//const UserRouter = require('./Routes/UserRouter');
+
+require('dotenv').config();
+require('./Config/db');
+
+const PORT = process.env.PORT || 8090;
+
+app.get('/', (req, res) => {
+    res.send('server is working');
+});
+app.use(bodyParser.json());
+app.use(cors());
+//app.use('/user', UserRouter);
+
+app.listen(PORT, () => {console.log(`Server is running on ${PORT}`)
+
+});
